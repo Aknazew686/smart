63f4ee037fd6e2146bf3e3648abaa7ce4f028ca7
Aknazew686 <Aknazew686@gmail.com>
2020-09-09 17:25:38 +0300

    вносит изменения

--- build/js/main.js
+++ build/js/main.js
@@ -1,56 +1,72 @@
-    var anchors = document.querySelectorAll('a[href*="#"]');
+
     var number = document.getElementById('phone');
     var call = document.querySelector('.page-header__link');
     var popup = document.querySelector('.popup');
     var popupToggle = document.querySelector('.popup__toggle');
     var siteList = document.querySelector('.sections-site__list');
     var siteListToggle = document.querySelector('.sections-site__toggle');
+    var siteListWrapper = document.querySelector('.sections-site__wrapper')
     var officeList = document.querySelector('.office__list');
     var officeToggle = document.querySelector('.office__toggle');
+    var officeWrapper = document.querySelector('.office__wrapper');
     var popupInputName = document.querySelector ('[name=name]');
     var popupInputPhone = document.querySelector ('[name=phone]');
     var popupInputMessage = document.querySelector ('[name=message]');
     var form = document.querySelector('.popup-form');
     var body = document.querySelector('body');
     var popupBackground = document.querySelector('.popup-background')
+    var body = document.querySelector('body');
+    var phone = document.querySelector('.fieldtext__input--phone');
+    var formm = document.querySelector('.form')
 
-    form.addEventListener("submit", function () {
-      localStorage.setItem("name", popupInputName.value);
-      localStorage.setItem("phone", popupInputPhone.value);
-      localStorage.setItem("message", popupInputMessage.value);
-    });
 
-    for (let anchor of anchors) {
-      anchor.addEventListener('click', function (event) {
-        event.preventDefault();
-        var blockID = anchor.getAttribute('href')
-        document.querySelector('' + blockID).scrollIntoView({
-          behavior: "smooth",
-          block: "start"
-        })
-      })
-    }
+    var validationPhone = function () {
+      if (phone.value.length < 10 ) {
+        phone.setCustomValidity('Должно быть введено 10 цифр номера ');
+      }   else {
+        phone.setCustomValidity('');
+      }
+    };
 
     call.addEventListener('click', function () {
       popup.classList.add('popup--active');
       popupBackground.classList.add('popup-background--active')
       popupInputName.focus();
+      body.classList.add('hidden');
     });
 
-    siteListToggle.addEventListener('click', function () {
+    form.addEventListener("submit", function (evt) {
+      localStorage.setItem("name", popupInputName.value);
+      localStorage.setItem("phone", popupInputPhone.value);
+      localStorage.setItem("message", popupInputMessage.value);
+
+    });
+
+    //formm.addEventListener("submit", function (evt) {
+     // evt.preventDefault();
+      //validationPhone();
+      //console.log(phone.value.length);
+      //formm.reset();
+    //});
+
+    siteListWrapper.addEventListener('click', function () {
       if (siteListToggle.classList.contains('sections-site__toggle--closed')) {
         siteListToggle.classList.remove('sections-site__toggle--closed');
         siteList.classList.remove('sections-site__list--closed');
+        officeList.classList.add('office__list--closed');
+        officeToggle.classList.add('office__toggle--closed');
       } else {
         siteListToggle.classList.add('sections-site__toggle--closed');
         siteList.classList.add('sections-site__list--closed');
       }
     });
 
-    officeToggle.addEventListener('click', function () {
+    officeWrapper.addEventListener('click', function () {
       if (officeToggle.classList.contains('office__toggle--closed')) {
         officeToggle.classList.remove('office__toggle--closed');
         officeList.classList.remove('office__list--closed');
+        siteListToggle.classList.add('sections-site__toggle--closed');
+        siteList.classList.add('sections-site__list--closed');
       } else {
         officeToggle.classList.add('office__toggle--closed');
         officeList.classList.add('office__list--closed');
@@ -62,6 +78,7 @@
         evt.preventDefault();
         popup.classList.remove('popup--active');
         popupBackground.classList.remove('popup-background--active');
+        body.classList.remove('hidden');
       }
     });
 
@@ -69,6 +86,7 @@
       popup.classList.remove('popup--active');
       popup.classList.add('popup--closed');
       popupBackground.classList.remove('popup-background--active')
+      body.classList.remove('hidden');
     });
 
     jQuery(function($){
@@ -76,13 +94,16 @@
         var popup = $(".popup"); // тут указываем ID элемента
         var call = $(".page-header__link");
         var popupBackground = $(".popup-background");
+        var body = $('body');
         if (!popup.is(e.target)
             && !call.is(e.target) // если клик был не по нашему блоку
             && popup.has(e.target).length === 0) { // и не по его дочерним элементам
           popup.removeClass('popup--active'); // скрываем его
           popupBackground.removeClass('popup-background--active');
+          body.removeClass('hidden');
         }
       });
     });
+
 
 
--- source/js/mask.js
+++ source/js/mask.js
@@ -2,3 +2,4 @@ new Inputmask({
 	mask: "+7 (999) 999 99 99",
 	showMaskOnHover: false,
 }).mask(document.querySelectorAll("[type='tel']"));
+
